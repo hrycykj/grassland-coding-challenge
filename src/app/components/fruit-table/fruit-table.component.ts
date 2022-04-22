f84032ffea9b298d64929e5dfa76088ef3af151d
hrycykj <john@quillconsulting.ca>
2022-04-21 19:46:39 -0600

    fruit filter functionality

--- src/app/components/fruit-table/fruit-table.component.ts
+++ src/app/components/fruit-table/fruit-table.component.ts
@@ -13,6 +13,29 @@ export class FruitTableComponent implements OnInit {
   constructor(public viewModel: FruitTableViewModel) {
 
   }
+  dataSource = this.viewModel.fruitData$
+  filteredDataSource = this.dataSource.value
+
+  fruitFilter(event: Event) {
+    console.log('event', event)
+    const filterBy = (event.target as HTMLInputElement).value.toLowerCase();
+    const dataTable = this.dataSource.value
+    let resultTable = []
+    console.log('filter term:',filterBy)
+    console.log('local dataTable',dataTable)
+    console.log('filteredData',this.filteredDataSource)
+    dataTable.forEach((fruitInfo) => {
+      // console.log ('for each:',fruitInfo)
+      if  (fruitInfo.genus.toLowerCase().includes(filterBy)
+          ||fruitInfo.name.toLowerCase().includes(filterBy)
+          ||fruitInfo.family.toLowerCase().includes(filterBy)
+          ||fruitInfo.order.toLowerCase().includes(filterBy)) {
+            resultTable.push(fruitInfo)
+      }
+      this.filteredDataSource=resultTable
+    })
+    console.log('filtered results',resultTable)
+  }
 
   ngOnInit(): void {
   }
